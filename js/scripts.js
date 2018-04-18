45a71aab293d62fb2dde90a47bb94b3bca0a502d
Ewa <grtxhu@gmail.com>
2018-04-18 22:38:15 +0200

    Add folders and files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+var a = prompt ('wpisz wartość a');
+alert('value a =' + a);
+var b = prompt ('wpisz wartość b');
+alert('value b =' + b);
+var value = (a * a) - (2 * a * b) - (b * b);
+
+if (value > 0) {
+  alert ('wartość dodatnia'); 
+  console.log('wartość dodatnia'); 
+}
+else if (value < 0) {
+  alert('wartość ujemna');
+  console.log('wartość ujemna');
+} 
+else {
+  alert('wartość równa 0');
+  console.log('Wynik równy zero');
+}
+
